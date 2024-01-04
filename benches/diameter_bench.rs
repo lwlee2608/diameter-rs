af8e21f241a8cf566304b30a76e81f9e657aa501
Jason Lee <lwlee2608@gmail.com>
2024-01-04 11:50:22 +0800

    add benchmark test

---
+++ benches/diameter_bench.rs
@@ -0,0 +1,24 @@
+#![feature(test)]
+
+extern crate test;
+use diameter::diameter::DiameterHeader;
+use test::black_box;
+use test::Bencher;
+
+#[bench]
+fn bench_decode(b: &mut Bencher) {
+    let data = test_data();
+    b.iter(|| black_box(DiameterHeader::decode_from(&data).unwrap()))
+}
+
+fn test_data() -> &'static [u8] {
+    return &[
+        0x01, 0x00, 0x00, 0x14, // version, length
+        0x80, 0x00, 0x01, 0x10, // flags, code
+        0x00, 0x00, 0x00, 0x04, // application_id
+        0x00, 0x00, 0x00, 0x03, // hop_by_hop_id
+        0x00, 0x00, 0x00, 0x04, // end_to_end_id
+    ];
+}
+
+fn main() {}
